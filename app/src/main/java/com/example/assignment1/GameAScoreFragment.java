55c8b1c8041e2c27b6642df219f86008f54ad000
Your Name <you@example.com>
2022-02-07 16:33:10 -0500

    update game A.

--- app/src/main/java/com/example/assignment1/GameAScoreFragment.java
+++ app/src/main/java/com/example/assignment1/GameAScoreFragment.java
@@ -4,23 +4,33 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link GameAScoreFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class GameAScoreFragment extends Fragment {
 
+    TextView finalscore_text;
+    int GAscore;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle bld = getArguments();
+        if (bld != null) {
+            GAscore = bld.getInt("GAscore");
+        } else {GAscore =0;}
 
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View gameALayout = inflater.inflate(R.layout.fragment_game_a_score, container, false);
+        finalscore_text = gameALayout.findViewById(R.id.Score_text);
+
+        finalscore_text.setText(GAscore);
         return gameALayout;
     }
 
@@ -29,7 +39,7 @@ public class GameAScoreFragment extends Fragment {
         view.findViewById(R.id.return_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Navigation.findNavController(view).navigate(R.id.action_gameAFragmentScore_to_secondFragment);
+                Navigation.findNavController(view).navigate(R.id.action_gameAScoreFragment_to_secondFragment);
             }
         });
 
